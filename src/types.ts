b2d43a16b479287826daddd0a0b961384404d16d
Zulkifli <aganzulkifli@gmail.com>
2026-05-06 18:40:50 +0700

    Tambah tipe data

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,30 @@
 // Hint: To-Do sebaiknya memiliki id, text, dan status completed
 
 // TODO: Buat interface untuk To-Do item
+interface ITodo {
+  id: number;
+  title: string;
+  description?: string;
+
+}
 
 // TODO: Buat tipe untuk status To-Do (active/done)
+type TTodo = ITodo & {
+  status: "active" | "done"
+}
+
 
 // TODO: Buat tipe untuk fungsi-fungsi yang akan digunakan
+
+type AddFunction = (todo: ITodo) => void;
+
+type MarkFunction = (id: number) => void;
+
+type DeleteFunction = (id: number) => void;
+
+type ListFunction = () => void;
+type SearchFunction = (keyword: string) => void;
+
+
+
+
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 // TODO: Implementasikan type guards di sini
 // Hint: Type guard berguna untuk memastikan tipe data saat runtime
 
+
 // TODO: Buat fungsi untuk memvalidasi apakah suatu objek adalah To-Do yang valid
 
 // TODO: Buat fungsi helper untuk menampilkan tanggal/waktu dengan format yang bagus
