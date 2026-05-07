7ac332cd331d5f2766b6532dd624cd1a862cdbc1
zulkiflidev <aganzulkifli@gmail.com>
2026-05-07 14:05:22 +0700

    Penambahan fungsi di utils.ts dan perbaikan di tsconfig.json

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,24 @@
 // TODO: Implementasikan type guards di sini
 // Hint: Type guard berguna untuk memastikan tipe data saat runtime
 
-
 // TODO: Buat fungsi untuk memvalidasi apakah suatu objek adalah To-Do yang valid
+
+function isValidTodo(todo: unknown): boolean {
+  // Pastikan todo adalah objek dan bukan null
+  if (typeof todo !== 'object' || todo === null) {
+    return false;
+  }
+
+  //sementara ke bentuk Record untuk mengakses propertinya dengan aman
+
+  const t = todo as Record<string, unknown>;
+  return (
+    typeof todo === 'object' &&
+    typeof todo.id === 'number' &&
+    typeof todo.title === 'string' &&
+    typeof todo.completed === 'boolean'
+  );
+}
 
 // TODO: Buat fungsi helper untuk menampilkan tanggal/waktu dengan format yang bagus
 
